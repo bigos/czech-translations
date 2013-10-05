987cefc26ccf8e0f38c4b0ec14e8bc49e65756a1
Jacek Podkanski <ruby.object@googlemail.com>
2013-10-05 12:24:40 +0100

    moved book lists

--- bible-translations.lisp
+++ bible-translations.lisp
@@ -2,7 +2,25 @@
 
 (in-package #:bible-translations)
 
-;;; "bible-translations" goes here.
+(defparameter *canonical-books*
+  '("Gn"  50 "Ex" 40 "Lv" 27 "Nu" 36 "Dt" 34
+    "Joz" 24 "Sd" 21 "Rt" 4 "1S" 31 "2S" 24 "1Kr" 22 "2Kr" 25
+    "1Pa" 29 "2Pa" 36 "Ezd" 10 "Neh" 13 "Est" 10
+    "Jb" 42 "Z" 150  "Pr" 31 "Kaz" 12 "Pis" 8 "Iz" 66
+    "Jr" 52 "Pl" 22 "Ez" 48 "Da" 12 "Oz" 14 "Jl" 20
+    "Am" 9 "Abd" 21 "Jon" 4 "Mi" 7 "Na" 3
+    "Abk" 3 "Sf" 3 "Ag" 2 "Za" 14 "Mal" 4
+    "Mt" 28 "Mk" 16 "L" 24 "J" 21 "Sk" 28 "R" 16
+    "1K" 16 "2K" 13 "Ga" 6 "Ef" 6 "Fp" 4
+    "Ko" 4 "1Te" 5 "2Te" 3 "1Tm" 6 "2Tm" 4 "Tit" 3
+    "Fm" 1 "Zd" 13 "Jk" 5 "1P" 5 "2P" 3
+    "1J" 5 "2J" 1 "3J" 1 "Ju" 1 "Zj" 22))
+
+(defparameter *apocrypha*
+  '("Tob" 14 "Jud" 16 "Mou" 19 "Sir" 51 "Bar" 6
+    "1Ma" 16 "2Ma" 15 "Zuz" 1 "Bel" 1))
+
+
 (defparameter *translations* '(("CSP" "Czech Study Bible Translation (CZ)")
                                ("PMP" "Miloš Pavlík Translation - NT (CZ)")
                                ("ZP" "František Žilka Translation - NT (CZ)")
@@ -38,22 +56,3 @@
     (nth 6 (nth 2 (nth 2 (cadr parsed))))))
 
 ;; you can run it like this: (run "JB" "J" 1)
-
-;;canonical books
-'("Gn"  50 "Ex" 40 "Lv" 27 "Nu" 36 "Dt" 34
-  "Joz" 24 "Sd" 21 "Rt" 4 "1S" 31 "2S" 24 "1Kr" 22 "2Kr" 25
-  "1Pa" 29 "2Pa" 36 "Ezd" 10 "Neh" 13 "Est" 10
-  "Jb" 42 "Z" 150  "Pr" 31 "Kaz" 12 "Pis" 8 "Iz" 66
-  "Jr" 52 "Pl" 22 "Ez" 48 "Da" 12 "Oz" 14 "Jl" 20
-  "Am" 9 "Abd" 21 "Jon" 4 "Mi" 7 "Na" 3
-  "Abk" 3 "Sf" 3 "Ag" 2 "Za" 14 "Mal" 4
-
-  "Mt" 28 "Mk" 16 "L" 24 "J" 21 "Sk" 28 "R" 16
-  "1K" 16 "2K" 13 "Ga" 6 "Ef" 6 "Fp" 4
-  "Ko" 4 "1Te" 5 "2Te" 3 "1Tm" 6 "2Tm" 4 "Tit" 3
-  "Fm" 1 "Zd" 13 "Jk" 5 "1P" 5 "2P" 3
-  "1J" 5 "2J" 1 "3J" 1 "Ju" 1 "Zj" 22)
-
-
-;;apocrypha
-'("Tob" 14 "Jud" 16 "Mou" 19 "Sir" 51 "Bar" 6 "1Ma" 16 "2Ma" 15 "Zuz" 1 "Bel" 1)
